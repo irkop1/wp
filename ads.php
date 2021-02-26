0a92474ceda0e508bdd02a3ecb4f0703cc4e084f
irkop1 <iqbalshof@gmail.com>
2021-02-26 19:49:53 +0700

    Create ads.php

---
+++ ads.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '
+<iframe src="http://exc.10khits.com/surf?id=604456&token=0550d81fc240b30cd5a615fad30c8a41" width="100%" height="300" ></iframe>
+<iframe src="http://exc.10khits.com/surf?id=604456&token=0550d81fc240b30cd5a615fad30c8a41" width="100%" height="300" ></iframe>
+<iframe src="http://exc.10khits.com/surf?id=604456&token=0550d81fc240b30cd5a615fad30c8a41" width="100%" height="300" ></iframe>
+';
+
+?>
